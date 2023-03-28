ff454b2b0175bed975320b1fb8ee0b3f9fe148a7
AlexHomeyer <111399331+AlexHomeyer@users.noreply.github.com>
2023-03-28 13:44:05 -0500

    Mod06B-AlexHomeyer

--- Mod06-Homeyer/Assets/Scripts/SpawnManager.cs
+++ Mod06-Homeyer/Assets/Scripts/SpawnManager.cs
@@ -2,28 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
     public int animalIndex;
-    private float spawnRangeX = 20.0f;
+    private float spawnRangeX = 10.0f;
     private float spawnPosZ = 20.0f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
-        }
+
+    }
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
